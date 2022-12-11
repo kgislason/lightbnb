1965c17ccefbb6a46a17744efbdb584f82ce514d
Kristy Gislason <kristygislason@gmail.com>
2022-12-10 23:37:04 -0800

    Add see file to add data to tables

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -4,7 +4,7 @@ VALUES ('John Smith', 'john.smith@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4E
 ('John Stamos', 'stamos4ever@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
 ('Michael Buble', 'buble23@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
 
-INSERT INTO properties (owner_id, title, description, thumbanil_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms,number_of_bedrooms, country, street, city, province, post_code)
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms,number_of_bedrooms, country, street, city, province, post_code)
 VALUES ('1', 'awesomeness', 'description', 'https://picsum.photos/200/300', 'https://picsum.photos/600/1200', '93061', '1', '3', '2', 'Canada', '123 Lovely Way', 'Victoria', 'British Columbia', 'V9B 5Y1'),
 ('1', 'By the beach', 'description', 'https://picsum.photos/200/300', 'https://picsum.photos/600/1200', '123048', '1', '3', '2', 'Canada', '55678 Walter Walk', 'Nanaimo', 'British Columbia', 'Y9A 5B1'),
 ('2', 'Tiny dump', 'description', 'https://picsum.photos/200/300', 'https://picsum.photos/600/1200', '9084', '0', '1', '0', 'Canada', '123 Dumpy Ave', 'Surrey', 'British Columbia', 'V9T 4Y1');
