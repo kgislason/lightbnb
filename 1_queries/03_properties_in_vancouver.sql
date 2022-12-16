774dfc235ab64657d34ef267ca6c13b7b367db34
Kristy Gislason <kristygislason@gmail.com>
2022-12-16 11:42:48 -0800

    Add comments to .sql SELECTs

--- 1_queries/03_properties_in_vancouver.sql
+++ 1_queries/03_properties_in_vancouver.sql
@@ -1,3 +1,9 @@
+-- Show specific details about properties located in Vancouver including their average rating
+-- Select the id, title, cost_per_night, and an average_rating from the properties table for properties located in Vancouver.
+-- Order the results from lowest cost_per_night to highest cost_per_night.
+-- Limit the number of results to 10.
+-- Only show listings that have a rating >= 4 stars.
+
 SELECT properties.id, title, cost_per_night, avg(property_reviews.rating) as average_rating
 FROM properties
 LEFT JOIN property_reviews ON properties.id = property_id
